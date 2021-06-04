6f7fba1b5687a738819f26f66bdf8a4578ff00a8
unlocomqx <amxmodx.unloco@gmail.com>
2021-06-04 10:09:17 +0100

    Remove render param

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,7 +3,7 @@ import { handle as shopifyHandle } from "./shopify/handler";
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle ({ request, render }) {
   if (request.path !== "/") {
-    const shopifyRes = await shopifyHandle({ request, render });
+    const shopifyRes = await shopifyHandle({ request });
     if (shopifyRes) {
       return shopifyRes;
     }
