e2c29af3a266bac90c055c6791d1c1708cf06e2c
unlocomqx <amxmodx.unloco@gmail.com>
2021-06-08 13:33:54 +0100

    Limit shopify auth routes

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,9 +2,12 @@ import { handle as shopifyHandle } from '$lib/shopify/handler';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle ({ request, render }) {
-	const shopifyRes = await shopifyHandle({ request });
-	if (shopifyRes) {
-		return shopifyRes;
+	const shopifyRoutes = ['/', '/auth', '/auth/inline', '/auth/callback'];
+	if (shopifyRoutes.includes(request.path)) {
+		const shopifyRes = await shopifyHandle({ request });
+		if (shopifyRes) {
+			return shopifyRes;
+		}
 	}
 
 	return render(request);
