cdae11e1fc776603d6b13028944ffe23972316ea
unlocomqx <amxmodx.unloco@gmail.com>
2021-06-08 15:25:09 +0100

    Persist session in a json file

--- src/lib/shopify/session.ts
+++ src/lib/shopify/session.ts
@@ -1,4 +1,50 @@
 import { Shopify } from '@shopify/shopify-api';
+import type { Session } from '@shopify/shopify-api/dist/auth/session';
+import fs from 'fs';
+// Saves the sesson to JSON
+// On prod save the session to the DB and use encryption
 
-// Extracted to separate module to use as a single instance
-export const sessionStorage = new Shopify.Session.MemorySessionStorage();
+const SESSION_FILE = 'sessions.json';
+
+const storeCallback = async (session: Session) => {
+	let sessions = {};
+	if (fs.existsSync(SESSION_FILE)) {
+		const json = fs.readFileSync(SESSION_FILE).toString();
+		sessions = JSON.parse(json || '{}');
+	}
+
+	sessions[session.id] = session;
+
+	fs.writeFileSync(SESSION_FILE, JSON.stringify(sessions, null, '\t'));
+
+	return true;
+};
+
+const loadCallback = async (id: string) => {
+	if (fs.existsSync(SESSION_FILE)) {
+		const json = fs.readFileSync(SESSION_FILE).toString();
+		const sessions = JSON.parse(json || '{}');
+		return sessions[id] || undefined;
+	}
+	return undefined;
+};
+
+const deleteCallback = async (id) => {
+	let sessions = {};
+	if (fs.existsSync(SESSION_FILE)) {
+		const json = fs.readFileSync(SESSION_FILE).toString();
+		sessions = JSON.parse(json || '{}');
+	}
+
+	delete sessions[id];
+
+	fs.writeFileSync(SESSION_FILE, JSON.stringify(sessions, null, '\t'));
+
+	return true;
+};
+
+export const sessionStorage = new Shopify.Session.CustomSessionStorage(
+	storeCallback,
+	loadCallback,
+	deleteCallback
+);
