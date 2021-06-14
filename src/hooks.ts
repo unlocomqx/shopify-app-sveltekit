b7236e392ea3617e1272cfabb002d96b6d4ef637
unlocomqx <amxmodx.unloco@gmail.com>
2021-06-14 22:24:33 +0100

    Add types to params

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,10 +1,14 @@
 import { handle as shopifyHandle } from '$lib/shopify/handler';
 import { verify } from '$lib/shopify/verify';
+import type { ServerRequest, ServerResponse } from '@sveltejs/kit/types/hooks';
 
 /** @type {import('@sveltejs/kit').Handle} */
-export async function handle ({ request, render }) {
+export async function handle<Locals> ({ request, render }: {
+	request: ServerRequest<Locals>;
+	render: (request: ServerRequest<Locals>) => ServerResponse | Promise<ServerResponse>;
+}) {
 	const shopifyRoutes = ['/', '/auth', '/auth/inline', '/auth/callback'];
-	console.log(request.path);
+
 	if (shopifyRoutes.includes(request.path)) {
 		const shopifyRes = await shopifyHandle({ request });
 		if (shopifyRes) {
